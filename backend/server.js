8ad63d5ac29abdbaddc4dcc8541793dc22bd3e95
presh-031 <akingbulugbe.dev@gmail.com>
2022-11-17 16:14:03 +0100

    setup folder structure

--- backend/server.js
+++ backend/server.js
@@ -11,5 +11,21 @@ dotenv.config();
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use("/", (req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+}); //custom logger
 
 const port = process.env.PORT || 4000;
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(port, () => {
+      console.log(`server running on port ${port}`);
+    })
+  )
+  .catch((err) => console.log(err.message));
+
+// routes
